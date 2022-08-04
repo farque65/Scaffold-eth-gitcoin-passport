ddc2b0f68d74b765651859e91334899dbda79e0d
Lucian Hymer <lucian.hymer@gmail.com>
2022-08-04 08:54:00 -0700

    Added verifier functionality, which required enabling WASM

--- packages/react-app/src/hooks/usePassport.js
+++ packages/react-app/src/hooks/usePassport.js
@@ -11,39 +11,116 @@ import { PassportReader } from "@gitcoinco/passport-sdk-reader";
   - @gitcoinco/passport-sdk-writer
 */
 
+// TODO
+// 1. Direct to passport if none exist yet
+// 2. Add stamp creation
+// 3. Move to a top-level context and let this be sourced throughout app
+
+const defaults = {
+  enabled: false,
+  active: false,
+  verified: false,
+  verifier: undefined,
+  doVerify: undefined,
+};
+
 function resetPassport() {
-  return { active: false };
+  return defaults;
+}
+
+function setPassport(data) {
+  return { ...defaults, ...data };
+}
+
+function errorPassport(data) {
+  return { ...defaults, error: data };
 }
 
 function updatePassport(state, action) {
-  const { type, data } = action;
+  const { type, data, verifier } = action;
   switch (type) {
-    case "disconnect":
+    case "reset":
       return resetPassport();
-    case "connect":
-      return { active: true, ...data };
+    case "enable":
+      return setPassport({ ...state, enabled: true });
+    case "disable":
+      return setPassport({ ...state, enabled: false });
+    case "toggle":
+      return setPassport({ ...state, enabled: !state.enabled });
+    case "update":
+      return setPassport({ ...state, ...data, active: true });
+    case "setVerifier":
+      return setPassport({ ...state, verifier });
+    case "initVerify":
+      return setPassport({ ...state, doVerify: true });
+    case "verify":
+      return setPassport({ ...state, ...data, verified: true });
+    case "error":
+      return errorPassport(data);
     default:
-      throw new Error("Invalid passport update");
+      return errorPassport("Invalid passport update");
   }
 }
 
-export default function usePassport(address, enabled) {
-  const [properties, dispatch] = useReducer(updatePassport, undefined, resetPassport);
+export default function usePassport(address) {
+  const [passport, dispatch] = useReducer(updatePassport, undefined, resetPassport);
+  const { doVerify, verifier, enabled } = passport;
 
-  useEffect(
-    () =>
-      (async () => {
-        if (address && enabled) {
-          const reader = new PassportReader("https://ceramic.passport-iam.gitcoin.co", "1");
-          const data = await reader.getPassport(address);
-          console.log("Passport Data", data);
-          dispatch({ type: "connect", data });
-        } else {
-          dispatch({ type: "disconnect" });
-        }
-      })(),
-    [address, enabled],
-  );
+  const enable = () => dispatch({ type: "enable" });
+  const disable = () => dispatch({ type: "disable" });
+  const toggle = () => dispatch({ type: "toggle" });
+  const verify = () => dispatch({ type: "initVerify" });
 
-  return { ...properties };
+  useEffect(() => {
+    const initVerifier = async () => {
+      if (doVerify) {
+        // Dynamically load @gitcoinco/passport-sdk-verifier
+        // Required for WASM
+        const PassportVerifier = (await import("@gitcoinco/passport-sdk-verifier")).PassportVerifier;
+        dispatch({
+          type: "setVerifier",
+          verifier: new PassportVerifier("https://ceramic.passport-iam.gitcoin.co", "1"),
+        });
+      }
+    };
+
+    initVerifier();
+  }, [doVerify]);
+
+  useEffect(() => {
+    const verifyPassport = async () => {
+      if (verifier && address && enabled && doVerify) {
+        const data = await verifier.verifyPassport(address);
+        console.log("verify data", data);
+
+        const failedStamps = data.stamps.filter(stamp => !stamp.verified);
+
+        if (failedStamps.length)
+          dispatch({
+            type: "error",
+            data: "Failed to verify stamp(s): " + JSON.stringify(failedStamps),
+          });
+        else dispatch({ type: "verify", data });
+      }
+    };
+
+    verifyPassport();
+  }, [verifier, address, enabled, doVerify]);
+
+  useEffect(() => {
+    const connectOrDisconnect = async () => {
+      if (address && enabled) {
+        const reader = new PassportReader("https://ceramic.passport-iam.gitcoin.co", "1");
+        const data = await reader.getPassport(address);
+        console.log("Passport Data", data);
+        dispatch({ type: "update", data });
+      } else {
+        dispatch({ type: "reset" });
+      }
+    };
+
+    connectOrDisconnect();
+  }, [address, enabled]);
+
+  return { verify, enable, disable, toggle, ...passport };
 }
--- packages/react-app/src/views/SigninPassport.jsx
+++ packages/react-app/src/views/SigninPassport.jsx
@@ -3,15 +3,14 @@ import GitcoinLogo from "../assets/GitcoinLogoWhite.svg";
 import usePassport from "../hooks/usePassport";
 
 export default function SigninPassport({ address }) {
-  const [passportEnabled, setPassportEnabled] = useState(false);
-  const passport = usePassport(address, passportEnabled);
+  const passport = usePassport(address);
 
   return (
     <div className="p-10">
       {/*
         ⚙️ Here is an example UI that displays and sets the purpose in your smart contract:
-      */}
-      <div className="border-2 border-solid border-gray-600 mx-auto w-1/2 bg-blue-darkblue text-white p-10">
+        */}
+      <div className="border-2 border-solid border-gray-600 mx-auto w-3/4 bg-blue-darkblue text-white p-10">
         <h2 className="text-white text-xl">Sign in with Passport</h2>
         <div className="mt-10 mb-4 flex items-center font-medium text-gray-900 md:mb-0">
           <img src={GitcoinLogo} alt="Gitcoin Logo White" />
@@ -22,51 +21,75 @@ export default function SigninPassport({ address }) {
             <button
               data-testid="connectWalletButton"
               className="rounded-sm rounded bg-purple-connectPurple py-2 px-10 text-white"
-              onClick={() => setPassportEnabled(!passportEnabled)}
+              onClick={() => passport.toggle()}
             >
-              {passport.active ? "Disconnect Wallet" : `Connect${passportEnabled ? "ing..." : " Wallet"}`}
+              {passport.active && passport.enabled
+                ? "Disconnect Wallet"
+                : `Connect${passport.enabled ? "ing..." : " Wallet"}`}
             </button>
+          )}
+          {passport.active && (
+            <>
+              <br />
+              <button
+                data-testid="verifyPassportButton"
+                className="rounded-sm rounded bg-purple-connectPurple py-2 px-10 text-white mt-4"
+                onClick={() => passport && passport.verify()}
+              >
+                Verif
+                {passport.verified ? "ied" : passport.doVerify ? "ing..." : "y"}
+              </button>
+            </>
           )}
         </div>
 
-        {passport.active && (
-          <div className="border-2 p-10 mt-20">
-            <h1 className="text-white text-3xl">Passport Data</h1>
-            {passport?.expiryDate && (
-              <p>
-                Expiry Date:{" "}
-                {
-                  // @ts-ignore
-                  passport?.expiryDate
-                }
-              </p>
-            )}
-            {passport?.issuanceDate && (
-              <p>
-                Issuance Date:{" "}
-                {
-                  // @ts-ignore
-                  passport?.issuanceDate
-                }
-              </p>
-            )}
+        {(passport.active || passport.error) && (
+          <div className="border-2 p-10 mt-10">
+            <h1 className="text-white text-3xl mb-0">
+              Passport {passport.active ? "Data" : "Error"}
+              {passport.verified && " \u2705"}
+              {passport.error && " \u274c"}
+            </h1>
+            {passport.error && passport.error}
+            {passport.active && (
+              <>
+                {passport?.expiryDate && (
+                  <p className="mt-4">
+                    Expiry Date:{" "}
+                    {
+                      // @ts-ignore
+                      passport?.expiryDate
+                    }
+                  </p>
+                )}
+                {passport?.issuanceDate && (
+                  <p>
+                    Issuance Date:{" "}
+                    {
+                      // @ts-ignore
+                      passport?.issuanceDate
+                    }
+                  </p>
+                )}
 
-            {passport?.stamps?.length > 0 && (
-              <div>
-                Stamps:{" "}
-                <ul className="list-disc ml-10">
-                  {
-                    // @ts-ignore
-                    passport?.stamps?.map(item => {
-                      return (
-                        <li key={item.provider} className="text-white">
-                          {item.provider}
-                        </li>
-                      );
-                    })
-                  }
-                </ul>
-              </div>
+                {passport?.stamps?.length > 0 && (
+                  <div>
+                    Stamps:{" "}
+                    <ul className="list-disc ml-10">
+                      {
+                        // @ts-ignore
+                        passport?.stamps?.map(item => {
+                          return (
+                            <li key={item.provider} className="text-white">
+                              {item.provider}
+                            </li>
+                          );
+                        })
+                      }
+                    </ul>
+                  </div>
+                )}
+              </>
             )}
           </div>
         )}
